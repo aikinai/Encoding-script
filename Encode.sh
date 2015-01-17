ab7af952e54bbc5455e5a53ae0b97abab79c5017
Alan <alan.rogers@nst.nintendo.com>
2015-01-17 00:41:15 -0800

    Refactor main encoding script
    Looks like I did this a long time ago but forgot to commit it.

--- Encode.sh
+++ Encode.sh
@@ -4,73 +4,137 @@
 PRESET="slower"
 PROFILE="high"
 LEVEL="41"
-RESOLUTION="720"
-BITRATE="1024k"
+RESOLUTION="1080"
+BITRATE="2048k"
+PIXELFORMAT="yuvj420p"
 
 # Audio parameters
 FREQUENCY="48"
-AUDIOBITRATE="64"
+AUDIOBITRATE="128"
 STRATEGY="VBRC"
 STRATEGY_NUM="2"
-CHANNELS="2"
+CHANNELS="1"
+
+case $CHANNELS in
+  1)
+    LAYOUT="Mono" ;;
+  2)
+    LAYOUT="Stereo" ;;
+  4)
+    LAYOUT="Quadraphonic" ;;
+  *)
+    echo -e "\x1B[00;31mError: Strange number of channels.\x1B[00m"
+    exit 1
+esac
+
+function cleanup()
+{
+  echo ""
+  echo -e "\x1B[00;32mRemoving temporary files.\x1B[00m"
+  # Remove temporary directory
+  [ -d tmp ] && rm -rf tmp
+  # Remove FFMPEG temporary files
+  ls ffmpeg2* &> /dev/null && rm -rf ffmpeg2*
+}
+
+# Always remove temporary files on exit
+trap cleanup EXIT
+
+# Create temporary and output directories
+[ -d tmp ] || mkdir tmp
+[ -d MP4 ] || mkdir MP4
 
 for INPUT in $@
 do
-    NAME=${INPUT%.*}
-    
-    VIDEO="NoAudio/${NAME}.mp4"
-    AUDIOSOURCE="NoAudio/${NAME}.wav"
-    AUDIO="NoAudio/${NAME}.m4a"
+  NAME=${INPUT%.*}
 
-    OUTPUT="MP4/${NAME}.mp4"
-    OUTPUT_FASTSTART="MP4/${NAME}_faststart.mp4"
+  VIDEO="tmp/${NAME}.mp4"
+  AUDIOSOURCE="tmp/${NAME}.wav"
+  AUDIO="tmp/${NAME}.m4a"
 
-    # Encode video
-    ffmpeg -i $INPUT -c:v libx264 \
-        -vf scale=-1:$RESOLUTION \
-        -preset $PRESET -profile:v $PROFILE -b:v $BITRATE -level:v $LEVEL \
-        -refs 6 \
-        -pix_fmt yuv420p \
-        -an -pass 1 -threads 4 -y $VIDEO 
+  OUTPUT="MP4/${NAME}.mp4"
+  OUTPUT_FASTSTART="MP4/${NAME}_faststart.mp4"
 
-    ffmpeg -i $INPUT -c:v libx264 \
-        -vf scale=-1:$RESOLUTION \
-        -preset $PRESET -profile:v $PROFILE -b:v $BITRATE -level:v $LEVEL \
-        -refs 6 \
-        -pix_fmt yuv420p \
-        -an \
-        -pass 2 -threads 4 -y $VIDEO
+  echo -e ""
+  echo -e "\x1B[00;32mEncoding ${INPUT}\x1B[00m" >&2
 
-    # Make audio source
-    ffmpeg -i $INPUT \
-        -c:a pcm_s16le -vn -y \
-        -ac $CHANNELS
-        $AUDIOSOURCE
 
-    # Encode audio
-    afconvert -v \
-        -f m4af -q 127 \
-        -d aac@${FREQUENCY}'000' \
-        -b ${AUDIOBITRATE}'000' \
-        -s $STRATEGY_NUM \
-        $AUDIOSOURCE \
-        $AUDIO
+  # Encode video
+  ffmpeg -i $INPUT -c:v libx264 \
+    -preset $PRESET -profile:v $PROFILE -b:v $BITRATE -level:v $LEVEL \
+    -refs 6 \
+    -pix_fmt $PIXELFORMAT \
+    -an \
+    -pass 1 -threads 4 -y $VIDEO 
+  
+  if [ $? -ne 0 ]; then
+    echo -e "\x1B[00;31mError: Failed to encode video.\x1B[00m" >&2
+    exit 1
+  fi
 
-    # Mux audio and video
-    ffmpeg \
-        -i $VIDEO \
-        -i $AUDIO \
-        -map 0:0 -map 1:0 \
-        -c:v copy -c:a copy \
-        -y \
-        $OUTPUT
+  ffmpeg -i $INPUT -c:v libx264 \
+    -preset $PRESET -profile:v $PROFILE -b:v $BITRATE -level:v $LEVEL \
+    -refs 6 \
+    -pix_fmt $PIXELFORMAT \
+    -an \
+    -pass 2 -threads 4 -y $VIDEO
 
-    # Run faststart to put the ATOM at the beginning for streaming
-    qt-faststart $OUTPUT $OUTPUT_FASTSTART
-    mv $OUTPUT_FASTSTART $OUTPUT
+  if [ $? -ne 0 ]; then
+    echo -e "\x1B[00;31mError: Failed to encode video.\x1B[00m" >&2
+    exit 1
+  fi
+
+  # Make audio source
+  ffmpeg -i $INPUT \
+    -c:a pcm_s16le -vn -y \
+    -ac $CHANNELS \
+    $AUDIOSOURCE
+
+  if [ $? -ne 0 ]; then
+    echo -e "\x1B[00;31mError: Failed to convert audio to lossless source.\x1B[00m" >&2
+    exit 1
+  fi
+
+  # Encode audio
+  afconvert -v \
+    -f m4af -q 127 \
+    -d aac@${FREQUENCY}'000' \
+    -b ${AUDIOBITRATE}'000' \
+    -s $STRATEGY_NUM \
+    -c $CHANNELS \
+    -l $LAYOUT \
+    $AUDIOSOURCE \
+    $AUDIO
+
+  if [ $? -ne 0 ]; then
+    echo -e "\x1B[00;31mError: Failed to encode audio.\x1B[00m" >&2
+    exit 1
+  fi
+
+  # Mux audio and video
+  ffmpeg \
+    -i $VIDEO \
+    -i $AUDIO \
+    -map 0:0 -map 1:0 \
+    -c:v copy -c:a copy \
+    -y \
+    $OUTPUT
+
+  if [ $? -ne 0 ]; then
+    echo -e "\x1B[00;31mError: Failed to multiplex audio and video.\x1B[00m" >&2
+    exit 1
+  fi
+
+  # Run faststart to put the ATOM at the beginning for streaming
+  qt-faststart $OUTPUT $OUTPUT_FASTSTART
+
+  if [ $? -ne 0 ]; then
+    echo -e "\x1B[00;31mError: Failed to move ATOM to beginning of file.\x1B[00m" >&2
+    exit 1
+  fi
+
+  mv $OUTPUT_FASTSTART $OUTPUT || exit 1
 
 done
-
-rm ffmpeg2*
 
 exit 0
