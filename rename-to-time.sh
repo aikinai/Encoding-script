d2fa2b7d02a3fdd6d8d30e390dad58898c2f2470
Alan Rogers <alanrogers@google.com>
2016-02-20 23:19:32 +0900

    Add script to rename files based on creation time

---
+++ rename-to-time.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# 
+# rename-to-time.sh
+# Renames files based on their creation date
+# Date format is a slightly modified ISO 8601
+#
+
+for FILE in $@
+do
+  # Get the modification date and convert it to ISO 8601 (simple time) format
+  DATETIME=$(date --date="$(GetFileInfo -d "${FILE}")" -Iseconds | sed 's/://g')
+  # Get the extension
+  EXTENSION="${FILE##*.}"
+  # Convert the extension to lowercase
+  EXTENSION="${EXTENSION,,}"
+  # Rename file to the ISO 8601 date
+  mv "${FILE}" "${DATETIME}.${EXTENSION}"
+done
