281612f398db7cd5306e88965d00b03fb0cec02e
Alan Rogers <alanrogers@google.com>
2016-09-18 16:30:31 +0900

    Add more metadata options to mts-to-mp4.sh

--- mts-to-mp4.sh
+++ mts-to-mp4.sh
@@ -2,11 +2,25 @@
 #
 # mts-to-mp4.sh
 # Converts MTS files to MP4 without transcoding
-# Copies all metadata
-# Applies created and modified dates to the new file
-# Will apply orientation metadata based on ROTATE variable
-#   ROTATE=CW will rotate clockwise
-#   ROTATE=CC will rotate counterclockwise
+# Copies as much original metadata and applies specified new metadata
+# Copies created and modified dates to the new file
+#
+# Optional new metadata:
+#   ROTATE
+#     CW will rotate clockwise
+#     CC will rotate counterclockwise
+#   START
+#     Time to start video copy
+#   STOP
+#     Time to stop video copy
+#   LOCATION
+#     A file to copy location from
+#   DESCRIPTION
+#     Description (caption)
+#   RATING
+#     Star rating (integer 1–5)
+#   KEYWORDS
+#     Comma-delimited list of keywords
 #
 
 # Set rotate argument as appropriate
@@ -32,9 +46,15 @@ fi
 
 for INPUT in $@
 do
+  # Use same filename for output
   OUTPUT="${INPUT%.*}.mp4"
+  # The MP4 spec calls for UTC time, so use that for the creation/encoding time
+  TIME_UTC="$(TZ=UTC stat -c '%y' "$INPUT" | sed 's/\.00000.*//')"
+  # Lightroom doesn't respect the UTC time spec, so use local time for Date/Time Original
+  TIME_LOCAL="$(stat -c '%y' "$INPUT" | sed 's/\.00000.*//')"
 
   # Copy video and audio to MP4
+  # with as much metadata as FFMPEG will copy
   TZ="UTC" \
   ffmpeg \
     ${START_ARG} \
@@ -45,20 +65,43 @@ do
     -map_metadata 0 \
     -map_metadata:s:v 0:s:v \
     -map_metadata:s:a 0:s:a \
-    -metadata creation_time="$(TZ=UTC stat -c '%y' "$INPUT" | sed 's/\.00000.*//')" \
+    -metadata creation_time="${TIME_UTC}" \
     ${ROTATE_ARG} \
     "$OUTPUT"
 
+  # Use exiftool to copy more metadata that FFMPEG misses
+  echo -e "\x1B[00;33mCopy all metadata from \x1B[01;35m${INPUT}\x1B[00m"
   exiftool -overwrite_original -TagsFromFile "$INPUT" "$OUTPUT"
+  # Set Date/Time Original to local time for Lightroom
+  echo -e "\x1B[00;33mSet Date/Time Original to (local time) \x1B[01;35m${TIME_LOCAL}\x1B[00m"
+  exiftool -overwrite_original -DateTimeOriginal="${TIME_LOCAL}" "$OUTPUT"
 
   if [ -n "$LOCATION" ]; then
+    echo -e "\x1B[00;33mCopy location from \x1B[01;35m${LOCATION}\x1B[00m"
     exiftool -overwrite_original -TagsFromFile "$LOCATION" -Location:all "$OUTPUT"
   fi
 
-  # Copy created and modified dates from mts
+  if [ -n "$DESCRIPTION" -o -n "$RATING" -o -n "$KEYWORDS" ]; then
+    EXIF_CMD="exiftool -overwrite_original"
+    if [ -n "$DESCRIPTION" ]; then
+      echo -e "\x1B[00;33mSet description to \x1B[01;35m${DESCRIPTION}\x1B[00m"
+      EXIF_CMD="${EXIF_CMD} -description=\"${DESCRIPTION}\""
+    fi
+    if [ -n "$RATING" ]; then
+      echo -e "\x1B[00;33mSet rating to \x1B[01;35m${RATING}\x1B[00m"
+      EXIF_CMD="${EXIF_CMD} -rating=${RATING}"
+    fi
+    if [ -n "$KEYWORDS" ]; then
+      echo -e "\x1B[00;33mSet keywords to \x1B[01;35m${KEYWORDS}\x1B[00m"
+      EXIF_CMD="${EXIF_CMD} -sep \", \" -keywords=\"${KEYWORDS}\""
+    fi
+    eval "$EXIF_CMD ${OUTPUT}"
+  fi
+
+  # Copy created and modified dates from original MTS
+  echo -e "\x1B[00;33mCopy file created and modified date from \x1B[01;35m${INPUT}\x1B[00m"
   SetFile \
     -d "$(GetFileInfo -d "$INPUT")" \
     -m "$(GetFileInfo -m "$INPUT")" \
     "$OUTPUT"
-
 done
