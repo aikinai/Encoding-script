768219351caaffcb6cd0a3b6b7122cc63e539831
Alan Rogers <alanrogers@google.com>
2018-10-24 20:57:06 +0900

    Update rename-to-time to use time from metadata
    It used to get the date from the file modified date, but metadata should
    be more robust. This will work at least with the MTS and MP4 files from
    my camera.

--- rename-to-time.sh
+++ rename-to-time.sh
@@ -1,18 +1,37 @@
 #!/usr/bin/env bash
 # 
 # rename-to-time.sh
-# Renames files based on their creation date
+# Renames video files based on their creation date
 # Date format is a slightly modified ISO 8601
 #
 
 for FILE in $@
 do
-  # Get the modification date and convert it to ISO 8601 (simple time) format
-  DATETIME=$(date --date="$(GetFileInfo -d "${FILE}")" -Iseconds | sed 's/://g')
-  # Get the extension
+
   EXTENSION="${FILE##*.}"
-  # Convert the extension to lowercase
-  EXTENSION="${EXTENSION,,}"
+  EXTENSION="${EXTENSION,,}" # Convert to lowercase
+  # MTS files have DateTimeOriginal set and MP4 files have LastUpdate
+  case "${EXTENSION}" in
+    mp4|MP4)
+      DATETIME="$(exiftool -s -s -s -d "%Y-%m-%dT%H%M%S%z" -LastUpdate ${FILE})"
+      FILEDATE="$(exiftool -s -s -s -d "%m/%d/%Y %H:%M:%S" -LastUpdate ${FILE})"
+      ;;
+    mts|MTS)
+      DATETIME="$(exiftool -s -s -s -d "%Y-%m-%dT%H%M%S%z" -DateTimeOriginal ${FILE})"
+      FILEDATE="$(exiftool -s -s -s -d "%m/%d/%Y %H:%M:%S" -DateTimeOriginal ${FILE})"
+      ;;
+  esac
+
   # Rename file to the ISO 8601 date
-  mv -iv "${FILE}" "${DATETIME}.${EXTENSION}"
+  NEWNAME="${DATETIME}.${EXTENSION}"
+  echo -e "Rename ${FILE} → \x1B[00;33m${NEWNAME}\x1B[00m"
+  mv -iv "${FILE}" "${NEWNAME}"
+
+  # Set creation and modification dates to the same time
+  echo -e "Set date → \x1B[00;33m${FILEDATE}\x1B[00m"
+  SetFile \
+    -d "${FILEDATE}" \
+    -m "${FILEDATE}" \
+    "${NEWNAME}"
+
 done
