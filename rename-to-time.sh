46cb9e1987742a18443754c5133d2494a43983e4
Alan Rogers <alanrogers@google.com>
2019-10-22 15:04:42 +0900

    Fix rename-to-time to work from other directories
    This had a lot of bugs that made it only work in the current directory.

--- rename-to-time.sh
+++ rename-to-time.sh
@@ -5,9 +5,9 @@
 # Date format is a slightly modified ISO 8601
 #
 
-for FILE in $@
+for FILE in "$@"
 do
-
+  DIRECTORY="$(dirname "${FILE}")"
   BASENAME="${FILE%.*}"
   EXTENSION="${FILE##*.}"
   EXTENSION="${EXTENSION,,}" # Convert to lowercase
@@ -16,16 +16,16 @@ do
   # determined from any particular spec.
   case "${EXTENSION}" in
     mp4|MP4)
-      DATETIME="$(exiftool -api largefilesupport=1 -s -s -s -d "%Y-%m-%dT%H%M%S%z" -LastUpdate ${FILE})"
-      FILEDATE="$(exiftool -api largefilesupport=1 -s -s -s -d "%m/%d/%Y %H:%M:%S" -LastUpdate ${FILE})"
+      DATETIME="$(exiftool -api largefilesupport=1 -s -s -s -d "%Y-%m-%dT%H%M%S%z" -LastUpdate "${FILE}")"
+      FILEDATE="$(exiftool -api largefilesupport=1 -s -s -s -d "%m/%d/%Y %H:%M:%S" -LastUpdate "${FILE}")"
       ;;
     mts|MTS)
-      DATETIME="$(exiftool -api largefilesupport=1 -s -s -s -d "%Y-%m-%dT%H%M%S%z" -DateTimeOriginal ${FILE})"
-      FILEDATE="$(exiftool -api largefilesupport=1 -s -s -s -d "%m/%d/%Y %H:%M:%S" -DateTimeOriginal ${FILE})"
+      DATETIME="$(exiftool -api largefilesupport=1 -s -s -s -d "%Y-%m-%dT%H%M%S%z" -DateTimeOriginal "${FILE}")"
+      FILEDATE="$(exiftool -api largefilesupport=1 -s -s -s -d "%m/%d/%Y %H:%M:%S" -DateTimeOriginal "${FILE}")"
       ;;
     mov|MOV)
-      DATETIME="$(exiftool -api largefilesupport=1 -s -s -s -d "%Y-%m-%dT%H%M%S%z" -CreateDate ${FILE})"
-      FILEDATE="$(exiftool -api largefilesupport=1 -s -s -s -d "%m/%d/%Y %H:%M:%S" -CreateDate ${FILE})"
+      DATETIME="$(exiftool -api largefilesupport=1 -s -s -s -d "%Y-%m-%dT%H%M%S%z" -CreateDate "${FILE}")"
+      FILEDATE="$(exiftool -api largefilesupport=1 -s -s -s -d "%m/%d/%Y %H:%M:%S" -CreateDate "${FILE}")"
       ;;
     xml|XML)
       break # XML files handled later to make sure the name matches perfectly
@@ -34,18 +34,19 @@ do
 
   # Rename file to the ISO 8601 date
   NEWNAME="${DATETIME}.${EXTENSION}"
+  DESTINATION="${DIRECTORY}"/"${NEWNAME}"
   echo -e "Rename ${FILE} → \x1B[00;33m${NEWNAME}\x1B[00m"
-  mv -iv "${FILE}" "${NEWNAME}"
+  mv -iv "${FILE}" "${DESTINATION}"
 
   # Rename matching XML file if it exists
   # The for loop is the best way to test if the file exists with globbing
   # XML files have names like `C0001M01.XML` to match `C0001.MP4` so I use the
   # globbing to be sure I get the matching XML file and rename with the exact
   # same time as the video.
-  for XMLFILE in ${BASENAME}*.XML; do
+  for XMLFILE in "${BASENAME}"*.XML; do
     if [ -e "$XMLFILE" ]; then
       echo -e "Rename ${XMLFILE} → \x1B[00;33m${DATETIME}.xml\x1B[00m"
-      mv -iv "${XMLFILE}" "${DATETIME}.xml"
+      mv -iv "${XMLFILE}" "${DIRECTORY}"/"${DATETIME}".xml
     fi
     # Don't actually need the loop, so break if it finds one
     break
@@ -56,6 +57,6 @@ do
   SetFile \
     -d "${FILEDATE}" \
     -m "${FILEDATE}" \
-    "${NEWNAME}"
+    "${DESTINATION}"
 
 done
